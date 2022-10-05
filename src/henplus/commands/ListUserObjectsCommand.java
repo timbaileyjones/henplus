a18e1581b880b8d198baa2f815daf23bac28042f
Joseph Mocker <mock@fakebelieve.org>
2022-10-05 16:55:30 -0700

    added ability to filter on schema and/or table. for example "public.*user*" or just "public." or just "*user*".

--- src/henplus/commands/ListUserObjectsCommand.java
+++ src/henplus/commands/ListUserObjectsCommand.java
@@ -16,24 +16,18 @@ import henplus.view.util.NameCompleter;
 import java.sql.Connection;
 import java.sql.DatabaseMetaData;
 import java.sql.ResultSet;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Set;
-import java.util.SortedSet;
+import java.util.*;
 
 /**
  * FIXME: use SQLMetaData stuff instead.
  */
 public class ListUserObjectsCommand extends AbstractCommand implements Interruptable {
 
-    private static final String[] LIST_TABLES_VIEWS = { "TABLE", "VIEW" };
-    private static final String[] LIST_TABLES = { "TABLE" };
-    private static final String[] LIST_VIEWS = { "VIEW" };
-    private static final int[] TABLE_DISP_COLS = { 2, 3, 4, 5 };
-    private static final int[] PROC_DISP_COLS = { 2, 3, 8 };
+    private static final String[] LIST_TABLES_VIEWS = {"TABLE", "VIEW"};
+    private static final String[] LIST_TABLES = {"TABLE"};
+    private static final String[] LIST_VIEWS = {"VIEW"};
+    private static final int[] TABLE_DISP_COLS = {2, 3, 4, 5};
+    private static final int[] PROC_DISP_COLS = {2, 3, 8};
 
     /**
      * all tables in one session.
@@ -56,7 +50,7 @@ public class ListUserObjectsCommand extends AbstractCommand implements Interrupt
      */
     @Override
     public String[] getCommandList() {
-        return new String[] { "tables", "views", "procedures", "rehash" };
+        return new String[]{"tables", "views", "procedures", "rehash"};
     }
 
     /**
@@ -98,7 +92,25 @@ public class ListUserObjectsCommand extends AbstractCommand implements Interrupt
                 renderer.getDisplayMetaData()[2].setAutoWrap(78);
 
                 if (param != null && !param.trim().isEmpty()) {
-                    renderer.setFilter(new GlobFilter(param.trim()));
+                    MultiGlobFilter multiFilter = new MultiGlobFilter();
+                    // foo.bar  = len 2
+                    // .bar = len 2
+                    // foo. = len 2
+                    String[] parts = param.trim().split("\\.", 2);
+                    if (parts.length == 2) {
+                        // we have a <schema>.<table> glob, either part could be empty
+                        if (!parts[0].isEmpty()) {
+                            multiFilter.addFilter(new GlobFilter(0, parts[0]));
+                        }
+                        if (!parts[1].isEmpty()) {
+                            multiFilter.addFilter(new GlobFilter(1, parts[1]));
+                        }
+                    } else {
+                        // only have a <table> glob
+                        multiFilter.addFilter(new GlobFilter(1, parts[0]));
+                    }
+
+                    renderer.setFilter(multiFilter);
                 }
 
                 final int tables = renderer.execute();
@@ -306,16 +318,18 @@ public class ListUserObjectsCommand extends AbstractCommand implements Interrupt
     public static class GlobFilter implements ResultSetRenderer.ResultSetFilter {
 
         private final String glob;
+        private final int col;
         private final String regex;
 
-        public GlobFilter(String glob) {
+        public GlobFilter(int col, String glob) {
+            this.col = col;
             this.glob = glob;
             this.regex = convertGlobToRegEx(this.glob);
         }
 
         @Override
         public boolean accept(String[] row) {
-           return row[1].matches(regex);
+            return row[col].matches(regex);
         }
 
         private String convertGlobToRegEx(String line) {
@@ -402,6 +416,26 @@ public class ListUserObjectsCommand extends AbstractCommand implements Interrupt
                 }
             }
             return sb.toString();
+        }
+    }
+
+    public static class MultiGlobFilter implements ResultSetRenderer.ResultSetFilter {
+
+        private List<GlobFilter> filters = new ArrayList<GlobFilter>();
+
+        public void addFilter(GlobFilter filter) {
+            filters.add(filter);
+        }
+
+        @Override
+        public boolean accept(String[] row) {
+            // if any of the filters return false, return false
+            for (GlobFilter filter : filters) {
+                if (!filter.accept(row)) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
